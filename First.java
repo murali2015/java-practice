280123707ff4b1aa9a7ae4a153e18261839ea2f0
murali krishna <muralikrishna25101997@gmail.com>
2020-08-25 18:55:08 +0530

    basic program

---
+++ First.java
@@ -0,0 +1,12 @@
+class First{
+public static void main (String args[]){
+System.out.println("hello telugu computer world!");
+for(int i=0; i<=10; i++){
+    if(i == 10){
+        System.out.println("my name is murali");
+    } else{
+        System.out.println("my name is mouli krishna");
+    }
+}
+}
+}
